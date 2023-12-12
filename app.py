41d5c414eaadc8366250653d197ea20589e5fea7
alvynabranches <alvynabranches@gmail.com>
2023-12-12 15:42:40 +0530

    U app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import os
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_methods=["*"])
 
 @app.get("/")
 async def home():
