121184e68f2b30a0412b3638a88174de144ae005
Alvyn Abranches <alvynabranches@gmail.com>
2024-01-26 09:13:38 +0530

    U db.py

--- db.py
+++ db.py
@@ -3,13 +3,14 @@ from sqlalchemy.ext.asyncio import create_async_engine
 
 db_file = "database.db"
 engine = create_engine(f"sqlite:///{db_file}", echo=True, future=True)
-async_engine = create_async_engine(
-    f"aiosqlite+sqlite:///{db_file}", echo=True, future=True
-)
+# async_engine = create_async_engine(
+#     f"aiosqlite+sqlite:///{db_file}", echo=True, future=True
+# )
 
 def create_tables():
     from models.user import User
     from models.hall import Hall, Seat
+    from models.feedback import Feedback
     from models.tiatr import Tiatr, Show
     from models.booking import Booking, Payment
     
