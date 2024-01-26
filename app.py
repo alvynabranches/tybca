0cbf69b18b40e40d272e5fa0f11b3405592a84ec
Alvyn Abranches <alvynabranches@gmail.com>
2024-01-26 08:20:06 +0530

    U app.py

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from routers.tiatr import tiatr_router
 
 app = FastAPI()
 app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_methods=["*"])
+app.include_router(tiatr_router, prefix="/tiatr")
 
 @app.get("/")
 async def home():
