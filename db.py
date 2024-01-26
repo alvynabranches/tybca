c3db55c6cfcc5c5fedd3489c86df15c9384bcd86
Alvyn Abranches <alvynabranches@gmail.com>
2024-01-26 08:19:13 +0530

    U db.py

--- db.py
+++ db.py
@@ -1,41 +1,14 @@
-import os
-from sqlalchemy.engine import URL
 from sqlmodel import create_engine, SQLModel
 from sqlalchemy.ext.asyncio import create_async_engine
 
-USERNAME = os.environ.get("USERNAME", "root")
-PASSWORD = os.environ.get("PASSWORD", "")
-DATABASE = os.environ.get("DATABASE", "tiatr")
-HOST = os.environ.get("HOST", "0.0.0.0")
-PORT = int(os.environ.get("PORT", "26258"))
-
-SQLALCHEMY_DATABASE_URL = URL.create(
-    "cockroachdb",
-    username=USERNAME,
-    password=PASSWORD,
-    host=HOST,
-    port=PORT,
-    database=DATABASE,
-    # query={"disable_cockroachdb_telemetry": "False"},
-)
-
-SQLALCHEMY_DATABASE_ASYNC_URL = URL.create(
-    "cockroachdb+asyncpg",
-    username=USERNAME,
-    password=PASSWORD,
-    host=HOST,
-    port=PORT,
-    database=DATABASE,
-    # query={"disable_cockroachdb_telemetry": "False"},
-)
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True, future=True)
+db_file = "database.db"
+engine = create_engine(f"sqlite:///{db_file}", echo=True, future=True)
 async_engine = create_async_engine(
-    SQLALCHEMY_DATABASE_ASYNC_URL, echo=True, future=True
+    f"aiosqlite+sqlite:///{db_file}", echo=True, future=True
 )
 
 def create_tables():
-    from models.user import User, Admin
+    from models.user import User
     from models.hall import Hall, Seat
     from models.tiatr import Tiatr, Show
     from models.booking import Booking, Payment
